9cffe3d4b42b478bbaaba3368216dad6bb2b2f8c
together08 <together08@yeah.net>
2022-06-05 10:41:02 +0800

    fixup! We don't need to have a check here

--- Android.mk
+++ Android.mk
@@ -1,13 +1,11 @@
 LOCAL_PATH := $(call my-dir)
-ifeq ($(ZODY_WITH_HMS), true)
-    include $(CLEAR_VARS)
-    LOCAL_MODULE := HuaweiAppMarket
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_SRC_FILES := prebuilt/HuaweiAppMarket.apk
-    LOCAL_CERTIFICATE := PRESIGNED
-    LOCAL_MODULE_CLASS := APPS
-    LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-    LOCAL_DEX_PREOPT := false
-    LOCAL_PRODUCT_MODULE := true
-    include $(BUILD_PREBUILT)
-endif
+include $(CLEAR_VARS)
+LOCAL_MODULE := HuaweiAppMarket
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := prebuilt/HuaweiAppMarket.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
